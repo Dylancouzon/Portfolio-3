81910aaaab220770963de8ef5c3c4c8647437b40
Dylancouzon <dylancouzon@gmail.com>
2021-05-11 17:30:46 -0700

    Populate the database

--- models/Projects.js
+++ models/Projects.js
@@ -5,8 +5,13 @@ const Schema = mongoose.Schema;
 const ProjectSchema = new Schema({
   name: String,
   description: String,
+  image: String,
   githubLink: String,
-  deployedLink: String
+  deployedLink: String,
+  main:{
+    type: Boolean,
+    default: 0
+  } 
 });
 
 const Book = mongoose.model("Book", ProjectSchema);
--- server.js
+++ server.js
@@ -14,7 +14,35 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/projects", { useNewUrlParser: true });
 
+db.Projects.insertMany([
+    {
+        name: "FarQuest",
+        description: "A role-playing game of medieval fantasy.<br><br>Select from a hero class of Knights, Wizards, Huntresses, or be an Assassin. Explore a rich world of story-telling, battles, and surprises as you journey to see the King.<br><br>",
+        image: "FarQuest.png",
+        githubLink: "https://github.com/Dylancouzon/farQuest",
+        deployedLink: "https://farquest.herokuapp.com/",
+        main: 1
+    },
+    {
+        name: "Covid-19 Vaccine Data",
+        image: "vaccinator.png",
+        githubLink: "https://github.com/Dylancouzon/GroupProject",
+        deployedLink: "https://dylancouzon.github.io/GroupProject"
+    },
+    {
+        name: "Work Day Scheduler",
+        image: "scheduler.png",
+        githubLink: "https://github.com/Dylancouzon/day-planner",
+        deployedLink: "https://dylancouzon.github.io/day-planner"
+    },
+    {
+        name: "Weather Dashboard",
+        image: "dashboard.png",
+        githubLink: "https://github.com/Dylancouzon/Weather-Dashboard",
+        deployedLink: "https://github.com/Dylancouzon/Weather-Dashboard"
+    }
+]);
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
